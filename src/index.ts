bd685a90473e0ff06efe079660cce6e7a0015c90
Jon Green <jongreen1996@gmail.com>
2025-03-17 15:04:04 +0000

    Add static file serving

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
+import { logger } from 'hono/logger';
 
 const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
-});
+app.use('*', logger());
 
 app.get('/api/test', (c) => {
   return c.text('This is a test endpoint!');
 });
+
+app.get('*', serveStatic({ root: './src/dist' }));
+app.get('*', serveStatic({ path: './src/dist/index.html' }));
 
 export default app;
