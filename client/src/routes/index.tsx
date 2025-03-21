b349adc19555311f70b785401081667e5bb50ce6
Jon Green <jongreen1996@gmail.com>
2025-03-21 09:16:50 +0000

    Add footer with event capture year and creator link

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -250,6 +250,23 @@ function App() {
           </div>
         </section>
       </main>
+
+      <footer className='mt-16 max-w-7xl mx-auto p-4 grid grid-cols-3 items-center justify-evenly'>
+        <div></div>
+        <p className='text-center text-muted-foreground'>
+          Event Capture - {new Date().getFullYear()}
+        </p>
+        <p className='text-right text-muted-foreground'>
+          Created by -{' '}
+          <a
+            href='https://jongreen.dev'
+            target='_blank'
+            className='underline text-blue-400'
+          >
+            jongreen.dev
+          </a>
+        </p>
+      </footer>
     </>
   );
 }
