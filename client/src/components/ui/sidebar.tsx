b3ae859f909aacd55bbd5d753d2d65f0f0f25e2b
Jon Green <jongreen1996@gmail.com>
2025-03-28 19:27:36 +0000

    Update deps

--- client/src/components/ui/sidebar.tsx
+++ client/src/components/ui/sidebar.tsx
@@ -205,7 +205,7 @@ function Sidebar({
 
   return (
     <div
-      className='group peer text-sidebar-foreground hidden md:block z-50'
+      className='group peer text-sidebar-foreground hidden md:block'
       data-state={state}
       data-collapsible={state === 'collapsed' ? collapsible : ''}
       data-variant={variant}
---
+++ src/dist/assets/index-DZ1_NvSc.js
@@ -0,0 +1,1 @@
+import{F as r,G as t}from"./index-BS9wj_6X.js";var a=t();const e=r(a);export{e as R,a as r};
