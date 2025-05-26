409cb62df73b733bd932cbbdb7413349e36704c5
Jon Green <jongreen1996@gmail.com>
2025-05-26 10:54:14 +0100

    Update build

--- src/dist/assets/x-B2Wi8uch.js
+++ src/dist/assets/x-B2Wi8uch.js
@@ -1,4 +1,4 @@
-import{c as o}from"./createLucideIcon-D4FR3i40.js";/**
+import{c as o}from"./createLucideIcon-T07JbB5r.js";/**
  * @license lucide-react v0.483.0 - ISC
  *
  * This source code is licensed under the ISC license.
